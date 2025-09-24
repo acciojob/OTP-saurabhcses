96a2c609952ea860a298d573aea23492005eab64
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-09-24 21:17:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+    inputs.forEach((input, index) => {
+      input.addEventListener('input', (e) => {
+        if (e.target.value) {
+
+         if (index < inputs.length - 1) {
+            inputs[index + 1].focus();
+          }
+        }
+      });
+
+      input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+          if (!e.target.value && index > 0) {
+            
+            inputs[index - 1].focus();
+            inputs[index - 1].value = '';
+          }
+        }
+      });
+    });
+
+    
+    inputs[0].focus();
